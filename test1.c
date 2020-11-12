ee8b978d1df2008bb7abb1097efcc115cd9b80d2
Huitailang <r350178982@126.com>
2020-11-12 21:47:47 +0800

    first commit

---
+++ test1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int feet, fathom;
+    
+    fathom = 2;
+    feet = 6 * fathom;
+    printf ("there are %d feet in %d fathom!", feet, fathom);
+
+    return 0;
+};
