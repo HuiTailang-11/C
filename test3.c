894f6a9d2f912916887cbcb71f070b002700e1c2
Huitailang <r350178982@126.com>
2020-11-14 23:44:24 +0800

    good night

---
+++ test3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int toes;
+    toes = 10;
+    printf("toes is %d \n", toes);
+    printf("toes power is %d \n", toes*toes);
+    printf("toes tribblets is %d \n", toes*toes*toes);
+
+    return 0;
+}
